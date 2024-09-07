a3783a223559061d47a983c19f4032d7d12c756c
Andrii <andriybilous121392@gmail.com>
2024-09-07 10:40:19 -0500

    test

--- src/constants/index.js
+++ src/constants/index.js
@@ -23,3 +23,4 @@ export const SMTP = {
 export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
 
 export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
+//test
